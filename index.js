971fab16a7a72ca6947b07c8c1ef2f12c9b2311e
Galal Mohamed <gel0o.m0o@gmail.com>
2025-09-12 18:17:08 +0300

    Initiated the first code in html, css and javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+const heading = document.getElementById("main-heading");
+
+const changeColor = () => {
+  heading.style.color = "blue";
+};
+
+const resetColor = () => {
+  heading.style.color = "#3d0301";
+};
